fd1bb0d7bae2831bdb7ed6018dc63bf3a4fd192f
Thibaut Stalin <thibaut.st@gmail.com>
2018-01-06 20:33:31 +0100

    install knpMenuBundle

---
+++ src/Menu/MenuBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Menu;
+
+use Knp\Menu\FactoryInterface;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     *
+     * Add any other dependency you need
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'wishlist_index'));
+        // ... add more children
+
+        return $menu;
+    }
+}
